adf24ade0888818b7452990a9f94dda6fbd995d3
Vuk Tacic <vuk.tacic@gmail.com>
2026-06-10 21:56:47 -0700

    Base functions

---
+++ mdcore/lib/tetris/tetris.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+
+namespace tetris {
+    void init_game() {
+    }
+
+    void loop_game() {
+        
+    }
+}
---
+++ mdcore/lib/tetris/tetris.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "config.h"
+#include <Arduino.h>
+
+namespace tetris {
+    extern void init_game();
+    extern void loop_game();
+}
